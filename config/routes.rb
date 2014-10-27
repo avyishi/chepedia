4d3e56850eea9c9ff5f3d9fca6b21363bf431c9d
avyishi <avyishi@gmail.com>
2014-10-27 00:40:03 -0700

    Updated wiki and added collaborators controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'collaborators/index'
+
+  get 'collaborators/new'
+
   devise_for :users
   resources :users, only: [:update]
   resources :wikis
-  
   resources :charges, :users
 
   get 'welcome/index'
