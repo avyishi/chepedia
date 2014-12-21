be943b3db6f6ee82c30b4cab4067c5fc44d0ea82
avyishi <avyishi@gmail.com>
2014-12-21 02:32:34 -0500

    Updated welcome page current user sign in display

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026010423) do
+ActiveRecord::Schema.define(version: 20141027073705) do
+
+  create_table "collaborators", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
